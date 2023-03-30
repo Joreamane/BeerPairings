7381d554445082c8db99b68ac9d108cd209f9885
Eric Fallon <ericmfal@gmail.com>
2023-03-30 15:03:22 -0700

    Adjusted add beer form

--- server/models/beer.model.js
+++ server/models/beer.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const BeerSchema = new mongoose.Schema({
     brand: String, // user input
     name: String, // user input
-    style: String //reference Style model
+    style: String, //user input
+    attributes: Array
 }, {timestamps: true})
 
 const beer = mongoose.model('beer', BeerSchema)
